6150a96c9e48b29dad1fb78b9802cc7fa9343c60
shaketohome <calltoshake@gmail.com>
2026-03-30 23:32:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let cart = [];
 
-function orderItem(item) {
+function orderItem(item){
   cart.push(item);
-  alert(item + " added to cart");
+  alert(item + " added");
 }
 
-function whatsappOrder() {
-  let message = "Hello, I want to order:\n" + cart.join(", ");
-  let url = "https://wa.me/919999999999?text=" + encodeURIComponent(message);
-  window.open(url, "_blank");
+function whatsappOrder(){
+  let msg = "Order:\n" + cart.join("\n");
+  let url = "https://wa.me/917702622925?text=" + encodeURIComponent(msg);
+  window.open(url);
 }
