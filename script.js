bbcfe47f4004a7c65ea0679e46bbe096575f3ff7
shaketohome <calltoshake@gmail.com>
2026-03-30 23:49:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function order(item) {
   let phone = "917702622925";
-  let message = "Hello, I want to order: " + item;
+  let text = "I want to order: " + item;
 
-  let url = "https://wa.me/" + phone + "?text=" + encodeURIComponent(message);
+  let url = "https://wa.me/" + phone + "?text=" + encodeURIComponent(text);
 
   window.open(url, "_blank");
 }
