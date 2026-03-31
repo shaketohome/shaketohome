acba734d8071c23774f58b7b795b1ce7525bfc25
shaketohome <calltoshake@gmail.com>
2026-03-31 22:44:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 let cart = [];
-let userLocation = "";
-
-function addToCart(name, price) {
-  cart.push({name, price});
-  document.getElementById("cartText").innerText = cart.length + " items";
-}
+let locationLink = "";
 
 function getLocation() {
   navigator.geolocation.getCurrentPosition(pos => {
     let lat = pos.coords.latitude;
     let lon = pos.coords.longitude;
 
-    userLocation = "https://maps.google.com/?q=" + lat + "," + lon;
-
-    document.getElementById("locationText").innerText = "📍 Location set";
+    locationLink = "https://maps.google.com/?q=" + lat + "," + lon;
+    document.getElementById("locationText").innerText = "📍 Location detected";
   });
 }
 
+function addToCart(name, price) {
+  cart.push({name, price});
+  document.getElementById("cartText").innerText = cart.length + " items";
+}
+
 function checkout() {
-  if(cart.length === 0){
-    alert("Cart empty");
-    return;
-  }
+  if(cart.length === 0) return alert("Cart empty");
+
+  showStatus("Order Accepted");
+  setTimeout(()=>showStatus("Preparing..."),2000);
+  setTimeout(()=>showStatus("On the way 🚴"),4000);
 
   let msg = "Order:%0A";
   let total = 0;
@@ -31,11 +31,16 @@ function checkout() {
     total += i.price;
   });
 
-  msg += "%0ATotal: ₹" + total;
-
-  if(userLocation){
-    msg += "%0ALocation: " + userLocation;
-  }
+  msg += "%0ATotal ₹" + total;
+  msg += "%0ALocation: " + locationLink;
 
   window.open("https://wa.me/917702622925?text=" + msg);
+}
+
+function showStatus(text){
+  let box = document.getElementById("statusBox");
+  let txt = document.getElementById("statusText");
+
+  box.classList.remove("hidden");
+  txt.innerText = text;
 }
